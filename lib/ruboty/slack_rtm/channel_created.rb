40c55b49d7e052da78baf7c42968db138cad5810
ima1zumi <mariimaizumi5@gmail.com>
2024-01-02 22:49:20 +0900

    First commit

---
+++ lib/ruboty/slack_rtm/channel_created.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "channel_created/version"
+
+module Ruboty
+  module SlackRtm
+    module ChannelCreated
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
