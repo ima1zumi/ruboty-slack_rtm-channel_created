75525375d64709bdfd68aecc6fbde0778bf4b87b
ima1zumi <mariimaizumi5@gmail.com>
2024-01-03 21:09:58 +0900

    Receive `channel_created` event and notify it to a channel.

--- lib/ruboty/slack_rtm/channel_created.rb
+++ lib/ruboty/slack_rtm/channel_created.rb
@@ -5,8 +5,29 @@ require_relative "channel_created/version"
 module Ruboty
   module SlackRtm
     module ChannelCreated
-      class Error < StandardError; end
-      # Your code goes here...
+      CHANNEL = ENV["SLACK_CHANNEL_CREATED_NOTIFY_CHANNEL"] || "#new_channel"
+
+      def on_channel_created(data)
+        channel_id = data["channel"]["id"]
+        message = "A new channel created: <##{channel_id}>"
+        attachments = [
+          {
+            fallback: message,
+            color: "good",
+            text: message
+          }
+        ]
+
+        channel = CHANNEL
+        channel = resolve_channel_id(channel[1..-1]) if channel.start_with?("#")
+        client.chat_postMessage(
+          channel: channel,
+          username: robot.name,
+          attachments: attachments.to_json
+        )
+      end
     end
+
+    ::Ruboty::Adapters::SlackRTM.include ChannelCreated if ::Ruboty::Adapters.const_defined?(:SlackRTM)
   end
 end
